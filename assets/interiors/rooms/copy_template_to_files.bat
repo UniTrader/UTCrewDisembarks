f17dfac8d394f1b155df570efe69e1e2666f31da
UniTrader <unitrader87@googlemail.com>
2015-04-08 18:21:17 +0200

    first git upload

---
+++ assets/interiors/rooms/copy_template_to_files.bat
@@ -0,0 +1,225 @@
+del interiors_rooms_dockingbay_alb_administration01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_administration01.xml
+
+del interiors_rooms_dockingbay_alb_ammo.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_ammo.xml
+
+del interiors_rooms_dockingbay_alb_ammo01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_ammo01.xml
+
+del interiors_rooms_dockingbay_alb_assettest.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_assettest.xml
+
+del interiors_rooms_dockingbay_alb_bar01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_bar01.xml
+
+del interiors_rooms_dockingbay_alb_bar02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_bar02.xml
+
+del interiors_rooms_dockingbay_alb_drones01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_drones01.xml
+
+del interiors_rooms_dockingbay_alb_trade01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_alb_trade01.xml
+
+del interiors_rooms_dockingbay_ammo.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ammo.xml
+
+del interiors_rooms_dockingbay_bar.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_bar.xml
+
+del interiors_rooms_dockingbay_build.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_build.xml
+
+del interiors_rooms_dockingbay_canterran_unique01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_canterran_unique01.xml
+
+del interiors_rooms_dockingbay_canterran_unique02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_canterran_unique02.xml
+
+del interiors_rooms_dockingbay_collisiontest.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_collisiontest.xml
+
+del interiors_rooms_dockingbay_drones.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_drones.xml
+
+del interiors_rooms_dockingbay_dv_administration01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_administration01.xml
+
+del interiors_rooms_dockingbay_dv_administration02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_administration02.xml
+
+del interiors_rooms_dockingbay_dv_bar01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_bar01.xml
+
+del interiors_rooms_dockingbay_dv_repairdock01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_repairdock01.xml
+
+del interiors_rooms_dockingbay_dv_repairdock02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_repairdock02.xml
+
+del interiors_rooms_dockingbay_dv_ship01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_ship01.xml
+
+del interiors_rooms_dockingbay_dv_ship02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_ship02.xml
+
+del interiors_rooms_dockingbay_dv_trade01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_dv_trade01.xml
+
+del interiors_rooms_dockingbay_ol_administration01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_administration01.xml
+
+del interiors_rooms_dockingbay_ol_ammo01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_ammo01.xml
+
+del interiors_rooms_dockingbay_ol_bar01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_bar01.xml
+
+del interiors_rooms_dockingbay_ol_build01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_build01.xml
+
+del interiors_rooms_dockingbay_ol_repairdock01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_repairdock01.xml
+
+del interiors_rooms_dockingbay_ol_ship01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_ship01.xml
+
+del interiors_rooms_dockingbay_ol_trade01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_ol_trade01.xml
+
+del interiors_rooms_dockingbay_reivers_unique01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_reivers_unique01.xml
+
+del interiors_rooms_dockingbay_reivers_unique02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_reivers_unique02.xml
+
+del interiors_rooms_dockingbay_repairdock.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_repairdock.xml
+
+del interiors_rooms_dockingbay_split_unique01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_split_unique01.xml
+
+del interiors_rooms_dockingbay_split_unique02.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_split_unique02.xml
+
+del interiors_rooms_dockingbay_testlevel.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_testlevel_data"></source>
+del interiors_rooms_dockingbay_trade.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_trade.xml
+
+del interiors_rooms_dockingbay_xenon_unique01.xml
+copy TEMPLATE_DIFF_Al_Bar.xml interiors_rooms_dockingbay_xenon_unique01.xml
+
+del macros\interiors_rooms_dockingbay_alb_administration01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_administration01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_administration01" />
+del macros\interiors_rooms_dockingbay_alb_ammo01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_ammo01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_ammo01" />
+del macros\interiors_rooms_dockingbay_alb_ammo_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_ammo_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_ammo" />
+del macros\interiors_rooms_dockingbay_alb_assettest_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_assettest_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_assettest" />
+del macros\interiors_rooms_dockingbay_alb_bar01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_bar01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_bar01" />
+del macros\interiors_rooms_dockingbay_alb_bar02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_bar02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_bar02" />
+del macros\interiors_rooms_dockingbay_alb_drones01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_drones01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_drones01" />
+del macros\interiors_rooms_dockingbay_alb_trade01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_alb_trade01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_alb_trade01" />
+del macros\interiors_rooms_dockingbay_ammo_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ammo_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ammo" />
+del macros\interiors_rooms_dockingbay_bar_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_bar_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_bar" />
+del macros\interiors_rooms_dockingbay_build_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_build_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_build" />
+del macros\interiors_rooms_dockingbay_canterran_unique01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_canterran_unique01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_canterran_unique01" />
+del macros\interiors_rooms_dockingbay_canterran_unique02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_canterran_unique02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_canterran_unique02" />
+del macros\interiors_rooms_dockingbay_collisiontest_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_collisiontest_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_collisiontest" />
+del macros\interiors_rooms_dockingbay_drones_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_drones_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_drones" />
+del macros\interiors_rooms_dockingbay_dv_administration01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_administration01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_administration01" />
+del macros\interiors_rooms_dockingbay_dv_administration02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_administration02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_administration02" />
+del macros\interiors_rooms_dockingbay_dv_bar01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_bar01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_bar01" />
+del macros\interiors_rooms_dockingbay_dv_repairdock01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_repairdock01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_repairdock01" />
+del macros\interiors_rooms_dockingbay_dv_repairdock02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_repairdock02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_repairdock02" />
+del macros\interiors_rooms_dockingbay_dv_ship01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_ship01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_ship01" />
+del macros\interiors_rooms_dockingbay_dv_ship02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_ship02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_ship02" />
+del macros\interiors_rooms_dockingbay_dv_trade01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_dv_trade01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_dv_trade01" />
+del macros\interiors_rooms_dockingbay_ol_administration01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_administration01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_administration01" />
+del macros\interiors_rooms_dockingbay_ol_ammo01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_ammo01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_ammo01" />
+del macros\interiors_rooms_dockingbay_ol_bar01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_bar01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_bar01" />
+del macros\interiors_rooms_dockingbay_ol_build01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_build01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_build01" />
+del macros\interiors_rooms_dockingbay_ol_repairdock01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_repairdock01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_repairdock01" />
+del macros\interiors_rooms_dockingbay_ol_ship01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_ship01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_ship01" />
+del macros\interiors_rooms_dockingbay_ol_trade01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_ol_trade01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_ol_trade01" />
+del macros\interiors_rooms_dockingbay_reivers_unique01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_reivers_unique01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_reivers_unique01" />
+del macros\interiors_rooms_dockingbay_reivers_unique02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_reivers_unique02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_reivers_unique02" />
+del macros\interiors_rooms_dockingbay_repairdock_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_repairdock_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_repairdock" />
+del macros\interiors_rooms_dockingbay_split_unique01_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_split_unique01_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_split_unique01" />
+del macros\interiors_rooms_dockingbay_split_unique02_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_split_unique02_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_split_unique02" />
+del macros\interiors_rooms_dockingbay_testlevel_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_testlevel_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_testlevel" />
+del macros\interiors_rooms_dockingbay_trade_macro.xml
+	Line 4:   <macro name="interiors_rooms_dockingbay_trade_macro" class="dockingbay">
+	Line 5:     <component ref="interiors_rooms_dockingbay_trade" />
+del macros\interiors_rooms_dockingbay_xenon_unique01_macro.xml
